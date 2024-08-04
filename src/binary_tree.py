218a570242225ff988529ed60852b053f1ce4fcc
Unai <unai.lopez@keepler.io>
2024-08-04 12:07:44 +0200

    removed some files that are not necessary

--- src/binary_tree.py
+++ src/binary_tree.py
@@ -24,237 +24,235 @@ from src.node import Node, NodeType
 
 warnings.filterwarnings('ignore', category=RuntimeWarning)
 
-class BinaryTree:
-    def __init__(self, max_initialization_depth: int, variables: List[str], unary_operators: List[str], binary_operators: List[str]):
-        self.max_initialization_depth = max_initialization_depth
-        self.variables = variables
-        self.unary_operators = unary_operators
-        self.binary_operators = binary_operators
-        self.operators = unary_operators + binary_operators
+def build_tree(
+    depth: int,
+    parent: dict,
+    variables: List[str],
+    unary_operators: List[str],
+    binary_operators: List[str]) -> dict:
 
-        self.loss = None
-        self.score = None
-        self.depth = None
+    node = {}
+    if depth < 1:
+        raise ValueError("Depth must be at least 1")
 
-        self.max_depth = random.randint(1, max_initialization_depth)
-        self.root = self._build_tree(self.max_depth)
-        self.executable_equation = None
-        self.nodes = None
-        self.complexity = None
+    if depth == 1:
+        node_value = random.choice(variables)
+        node[node_value] = {
+            "left": None,
+            "right": None
+        }
+        return node
+    else:
+        if depth > 2:
+            node_value = random.choice(binary_operators + unary_operators + variables)
+        elif depth == 2:
+            node_value = random.choice(unary_operators + variables)
 
-        self.update_tree_info()
+        node[node_value] = {
+            "left": None,
+            "right": None
+        }
+                
+        if node_value in unary_operators:
+            node[node_value]["right"] = build_tree(depth - 1, node, variables, unary_operators, binary_operators)
+        elif node_value in binary_operators:
+            node[node_value]["left"] = build_tree(depth - 1, node, variables, unary_operators, binary_operators)
+            node[node_value]["right"] = build_tree(depth - 1, node, variables, unary_operators, binary_operators)
 
-    def _update_nodes(self):
-        self.nodes = self._collect_nodes()
+        return node    
+
+def build_full_binary_tree(
+    max_initialization_depth: int,
+    variables: List[str],
+    unary_operators: List[str],
+    binary_operators: List[str]) -> dict:
+
+    full_binary_tree = {
+        "max_initialization_depth": max_initialization_depth,
+        "loss": None,
+        "score": None,
+        "max_depth": random.randint(1, max_initialization_depth),
+        "depth": None,
+        "executable_equation": None,
+        "complexity": None
+    }
+
+    tree = build_tree(
+        full_binary_tree["max_depth"],
+        {},
+        variables,
+        unary_operators,
+        binary_operators
+    )
+    full_binary_tree["tree"] = tree
+
+    return full_binary_tree
+
+def build_equation(node: dict, operators: List[str]) -> str:
+    node_value = list(node.keys())[0]
+    node_value_children = node[node_value]
+    if node_value in operators:
+        operation = str(OPERATIONS[node_value])
+    else:
+        operation = str(node_value)
     
-    def _update_complexity(self):
-        self.complexity = len(self.nodes)
+    if not node_value_children["left"] and not node_value_children["right"]:
+        return operation
+    elif node_value_children["right"] and not node_value_children["left"]:
+        right_expr = build_equation(node_value_children["right"], operators)
+        return f"({operation}({right_expr}))"
+    elif node_value_children["left"] and node_value_children["right"]:
+        left_expr = build_equation(node_value_children["left"], operators)
+        right_expr = build_equation(node_value_children["right"], operators)
+        return f"({operation}({left_expr}, {right_expr}))"
+    
+def build_executable_equation(equation: str, variables: List[str]) -> str:
+    substitutions = {}
+    for i, col in enumerate(variables):
+        substitutions[col] = f"X[:, {i}]"
 
-    def _build_tree(self, depth: int, parent: Node = None) -> Node:
-        if depth < 1:
-            raise ValueError("Depth must be at least 1")
+    executable_equation = equation
+    for var in variables:
+        if var in substitutions:
+            executable_equation = re.sub(r'\b' + re.escape(var) + r'\b', substitutions[var], executable_equation)
+    
+    return executable_equation
 
-        if depth == 1:
-            node_value = random.choice(self.variables)
-            if node_value == "const":
-                node_type = NodeType.CONSTANT
+def calculate_loss(
+    X: np.ndarray,
+    y: np.ndarray,
+    loss_function: Callable,
+    executable_equation: str) -> float:
+    
+    try:
+        loss = loss_function(y, eval(executable_equation))
+    except:
+        loss = np.inf
+    if math.isinf(loss) or math.isnan(loss):
+        loss = np.inf
+    return float(loss)
+    
+def calculate_score(
+    X: np.ndarray,
+    y: np.ndarray,
+    score_function: Callable,
+    executable_equation: str) -> float:
+
+    try:
+        score = score_function(y, eval(executable_equation))
+    except:
+        score = np.inf
+    if math.isinf(score) or math.isnan(score):
+        score = 0
+    return float(score)
+
+def get_complexity(node: dict) -> int:
+    if node is None:
+        return 0
+    
+    count = 1
+    left_child = list(node.values())[0]["left"]
+    right_child = list(node.values())[0]["right"]
+    
+    count += get_complexity(left_child)
+    count += get_complexity(right_child)
+    
+    return count
+
+def calculate_max_depth(tree: dict) -> int:
+    if tree == {}:
+        return 0
+
+    node = tree[list(tree.keys())[0]]
+    left_depth = calculate_max_depth(node["left"]) if node["left"] else 0
+    right_depth = calculate_max_depth(node["right"]) if node["right"] else 0
+    return max(left_depth, right_depth) + 1
+
+def update_tree_info(
+    tree: dict,
+    operators: List[str],
+    variables: List[str]) -> dict:
+
+    tree["complexity"] = get_complexity(tree["tree"])
+    tree["depth"] = calculate_max_depth(tree["tree"])
+    tree["equation"] = build_equation(tree["tree"], operators)
+    tree["executable_equation"] = build_executable_equation(tree["equation"], variables)
+
+def visualize_binary_tree(node: dict, variables: List[str]) -> None:
+    graph = graphviz.Digraph()
+    parent_node_id = str(uuid.uuid1())
+    graph.node(name=parent_node_id, label=str(list(node.keys())[0]), style="filled", fillcolor="red")
+
+    def add_nodes_edges(node: dict, node_id: str) -> None:
+        node_left_id = str(uuid.uuid1())
+        node_right_id = str(uuid.uuid1())
+        
+        node_value = list(node.keys())[0]
+        node_value_children = node[node_value]
+        if node_value_children["left"]:
+            node_value_left = list(node_value_children["left"].keys())[0]
+            if node_value_left not in variables + ["const"]:
+                left_fill_color = "red"
+                shape = None
             else:
-                node_type = NodeType.VARIABLE
+                left_fill_color = "green"
+                shape = "rectangle"
 
-            return Node(
-                id=uuid.uuid1(),
-                node_type=node_type,
-                value=node_value,
-                parent=parent
-            )
-        else:
-            if depth > 2:
-                node_value = random.choice(self.operators + self.variables)
-            elif depth == 2:
-                node_value = random.choice(self.unary_operators + self.variables)
 
-            if node_value in self.unary_operators:
-                node_type = NodeType.UNARY_OPERATOR
-            elif node_value in self.binary_operators:
-                node_type = NodeType.BINARY_OPERATOR
-            elif node_value == "const":
-                node_type = NodeType.CONSTANT
+            graph.node(name=node_left_id, label=node_value_left, shape=shape, style="filled", fillcolor=left_fill_color)
+            graph.edge(node_id, node_left_id)
+            add_nodes_edges(node_value_children["left"], node_left_id)
+        if node_value_children["right"]:
+            node_value_right = list(node_value_children["right"].keys())[0]
+            if node_value_right not in list(variables) + ["const"]:
+                right_fill_color = "red"
+                shape = None
             else:
-                node_type = NodeType.VARIABLE
-                 
-            node = Node(
-                id=uuid.uuid1(),
-                node_type=node_type,
-                value=node_value,
-                parent=parent
-            )
+                right_fill_color = "green"
+                shape = "rectangle"
 
-            if node_value in self.unary_operators:
-                node.right = self._build_tree(depth - 1, node)
-            elif node_value in self.binary_operators:
-                node.left = self._build_tree(depth - 1, node)
-                node.right = self._build_tree(depth - 1, node)
+            graph.node(name=node_right_id, label=node_value_right, shape=shape, style="filled", fillcolor=right_fill_color)
+            graph.edge(node_id, node_right_id)
+            add_nodes_edges(node_value_children["right"], node_right_id)
+
+    add_nodes_edges(node, parent_node_id)
+
+    #graph_path = "equation_tree.png"
+    #graph.render(graph_path, format="png", cleanup=True)
+    graph_data = graph.pipe(format="png")
+    img = Image.open(io.BytesIO(graph_data))
+    plt.figure(figsize=(10, 10))
+    plt.imshow(img)
+    plt.axis("off")
+    plt.show()
+
+if __name__ == "__main__":
+    from pprint import pprint
+
+    variables = ["x0", "x1"]
+    unary_operators = ["sin", "cos", "tan", "log", "abs", "exp"]
+    binary_operators = ["+", "-", "*", "**", "/"]
+
+    for _ in range(10):
+        my_binary_tree = build_full_binary_tree(7, variables, unary_operators, binary_operators)
+        print("THIS IS THE TREE")
+        pprint(my_binary_tree)
+
+        print("THIS IS THE NUMPY EQUATION")
+        equation = build_equation(my_binary_tree["tree"], unary_operators + binary_operators)
+        print(equation)
+
+        print("THIS IS THE EXECUTABLE NUMPY EQUATION")
+        executable_equation = build_executable_equation(equation, variables)
+        print(executable_equation)
+
+        print("TREE DEPTH")
+        print(calculate_max_depth(my_binary_tree["tree"]))
+
+        print("COMPLEXITY")
+        print(get_complexity(my_binary_tree["tree"]))
+
+        print("VISUALIZE TREE")
+        visualize_binary_tree(my_binary_tree["tree"], variables)
+
         
-            return node
-
-    def _collect_nodes(self):
-        if not self.root:
-            return []
-        
-        nodes = []
-        queue = deque([self.root])
-        while queue:
-            node = queue.popleft()
-            nodes.append(node)
-            if node.left:
-                queue.append(node.left)
-            if node.right:
-                queue.append(node.right)
-        
-        return nodes
-
-    def evaluate(self, X: np.ndarray) -> float:
-        return self.root.evaluate(X)
-
-    def _build_equation(self, node: Node) -> None:
-        if node.value in self.operators:
-            operation = str(OPERATIONS[node.value])
-        else:
-            operation = str(node.value)
-        
-        if not node.left and not node.right:
-            return operation
-        elif node.right and not node.left:
-            right_expr = self._build_equation(node.right)
-            return f"({operation}({right_expr}))"
-        elif node.left and node.right:
-            left_expr = self._build_equation(node.left)
-            right_expr = self._build_equation(node.right)
-            return f"({operation}({left_expr}, {right_expr}))"
-
-    def perform_mutation(self) -> None:
-        node_to_mutate = random.choice(self.nodes)
-        if node_to_mutate.node_type == NodeType.UNARY_OPERATOR:
-            temp_operators = self.unary_operators.copy()
-            temp_operators.remove(node_to_mutate.value)
-        elif node_to_mutate.node_type == NodeType.BINARY_OPERATOR:
-            temp_operators = self.binary_operators.copy()
-            temp_operators.remove(node_to_mutate.value)
-        elif node_to_mutate.node_type == NodeType.VARIABLE:
-            temp_operators = self.variables.copy()
-            temp_operators.remove(node_to_mutate.value)
-
-        mutated_node_value = random.choice(temp_operators)
-        node_to_mutate.value = mutated_node_value
-
-    def perform_subtree_mutation(self):
-        #TODO
-        """
-        Selection of Mutation Point:
-        Randomly select a node in the binary tree. This node will be the root of the subtree that will be replaced.
-
-        Generation of New Subtree:
-        Generate a new random subtree. This subtree can be generated using the same rules and functions that were used to create the initial population of trees.
-
-        Replacement:
-        Replace the selected node and its subtree with the new randomly generated subtree.
-        """
-        pass
-        
-
-    def perform_node_mutation(self):
-        #TODO
-        """
-        Randomly select a node in the tree and replace its value with another valid value (e.g., replace an operator with another operator or a variable with another variable).
-        """
-        pass
-    
-    def perform_hoist_mutation(self):
-        #TODO
-        """
-        Select a random subtree and replace the entire tree with this subtree, effectively "hoisting" it up to the root.
-        """
-        pass
-
-    def perform_shrink_mutation(self):
-        #TODO
-        """
-        Select a subtree and replace it with one of its subtrees, effectively shrinking the tree.
-        """
-        pass
-
-    def _build_executable_equation(self) -> None:
-        substitutions = {}
-        for i, col in enumerate(self.variables):
-            substitutions[col] = f"X[:, {i}]"
-
-        executable_equation = self.equation
-        for var in self.variables:
-            if var in substitutions:
-                executable_equation = re.sub(r'\b' + re.escape(var) + r'\b', substitutions[var], executable_equation)
-        
-        self.executable_equation = executable_equation
-
-    def update_tree_info(self) -> None:
-        self._update_nodes()
-        self._update_complexity()
-        self.depth = self.root.calculate_max_depth()
-        self.equation = self._build_equation(self.root)
-        self._build_executable_equation()
-
-    def calculate_loss(self, X: np.ndarray, y: np.ndarray, loss_function: Callable) -> None:
-        try:
-            loss = loss_function(y, eval(self.executable_equation))
-        except:
-            loss = np.inf
-        if math.isinf(loss) or math.isnan(loss):
-            loss = np.inf
-        self.loss = float(loss)
-    
-    def calculate_score(self, X: np.ndarray, y: np.ndarray, score_function: Callable) -> None:
-        try:
-            score = score_function(y, eval(self.executable_equation))
-        except:
-            score = np.inf
-        if math.isinf(score) or math.isnan(score):
-            score = 0
-        self.score = float(score)
-
-    def visualize_binary_tree(self) -> None:
-        graph = graphviz.Digraph()
-        graph.node(name=str(self.root.id), label=str(self.root.value), style="filled", fillcolor="red")
-
-        def add_nodes_edges(node):
-            if node.left:
-                if node.left.value not in list(self.variables) + ["const"]:
-                    left_fill_color = "red"
-                    shape = None
-                else:
-                    left_fill_color = "green"
-                    shape = "rectangle"
-
-                graph.node(name=str(node.left.id), label=str(node.left.value), shape=shape, style="filled", fillcolor=left_fill_color)
-                graph.edge(str(node.id), str(node.left.id))
-                add_nodes_edges(node.left)
-            if node.right:
-                if node.right.value not in list(self.variables) + ["const"]:
-                    right_fill_color = "red"
-                    shape = None
-                else:
-                    right_fill_color = "green"
-                    shape = "rectangle"
-
-                graph.node(name=str(node.right.id), label=str(node.right.value), shape=shape, style="filled", fillcolor=right_fill_color)
-                graph.edge(str(node.id), str(node.right.id))
-                add_nodes_edges(node.right)
-
-        add_nodes_edges(self.root)
-
-        #graph_path = 'equation_tree.png'
-        #graph.render(graph_path, format='png', cleanup=True)
-        graph_data = graph.pipe(format='png')
-        img = Image.open(io.BytesIO(graph_data))
-        plt.figure(figsize=(10, 10))
-        plt.imshow(img)
-        plt.axis('off')
-        plt.show()
